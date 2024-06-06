0e4458927573bdea78f8f5e8ce42d0c322cca9e6
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2024-06-06 21:35:19 +0300

    admins Auth

--- routes/admin.php
+++ routes/admin.php
@@ -1,9 +1,7 @@
 <?php
 
+    use App\Http\Controllers\Admin\AdminAuthController;
     use Illuminate\Support\Facades\Route;
 
 
-
-    Route::get('/admin' , function () {
-        return view('admin.dashboard.index');
-    })->name('admin.dashboard.index');
+    Route::get('admin/login' , [ AdminAuthController::class , 'login' ])->name('admin.auth');
