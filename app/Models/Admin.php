658ab8b52d4ecacea1db2a67597420910bb19d86
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2024-06-08 03:01:27 +0300

    admin_gards

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,12 +3,11 @@
     namespace App\Models;
 
     use Illuminate\Database\Eloquent\Factories\HasFactory;
-    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Foundation\Auth\User as Authenticatable;
     use Illuminate\Notifications\Notifiable;
     use Laravel\Sanctum\HasApiTokens;
 
-
-    class Admin extends Model
+    class Admin extends Authenticatable
     {
         use HasApiTokens , HasFactory , Notifiable;
 
@@ -17,6 +16,8 @@
          *
          * @var array<int, string>
          */
+        protected $guard = "admin";
+
         protected $fillable = [
             'name' ,
             'email' ,
@@ -24,7 +25,6 @@
             'image' ,
             'status' ,
         ];
-        protected  $guard="admin";
 
         /**
          * The attributes that should be hidden for serialization.
